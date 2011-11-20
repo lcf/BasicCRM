2e563dd2c9d1e6c6749c824c97aba6acdf38fe01
Alexander Steshenko <lcfsoft@gmail.com>
2011-11-20 21:26:47 +0400

    twitter bootstrap layout applied, about page added

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,6 +3,11 @@ class IndexController extends Zend_Controller_Action
 {
     public function indexAction()
     {
-        
+        $this->render('about');
+    }
+
+    public function aboutAction()
+    {
+
     }
 }
